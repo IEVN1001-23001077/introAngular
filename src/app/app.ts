dc460e52fdab146ceb65e8b1a05aae52d5232467
SGordillo <85625@alumnos.utleon.edu.mx>
2026-09-12 09:59:26 -0600

    Heroes 12/09/26

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,16 @@ import { Component, signal } from '@angular/core';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('IntroAngular');
+  title = "Juanito";
+
+  duplicanumero(n1:number):number{
+    return n1*2
+  }
+
+  pelicula={
+    titulo:'Spider Man',
+    fechaLanzamiento:new Date(),
+    precio:1234
+
+  }
 }
